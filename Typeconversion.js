aa61730c97cf7a68bd2c440a6855dc718ea1c2de
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-02-23 20:47:54 +0600

    type conversation

---
+++ Typeconversion.js
@@ -0,0 +1,15 @@
+var str = '1000'
+var n = 10
+
+
+
+
+
+
+console.log(Number.parseInt(str)); 
+console.log(n.toString()) ;
+console.log(Number.parseInt(str));
+console.log(Boolean(-Infinity));
+console.log(Boolean(''))
+console.log(Boolean('apple'))
+console.log(Boolean(null))
