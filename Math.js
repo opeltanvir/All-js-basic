7ab909ab2838c620b3f2a0fbadde3cbd066e5993
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-02-27 11:18:22 +0600

    Math

---
+++ Math.js
@@ -0,0 +1,16 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 4.589
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+console.log(Math.max(400,500,600))
+console.log(Math.min(400,500,200))
+console.log(Math.pow(3,5))
+console.log(Math.sqrt(9))
+console.log(Math.random()*50+1)
+console.log(Math.round(Math.random()*50+1))
+
+
