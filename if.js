abac679a8bab43e30b566fbc5fa25a6854664589
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-02-27 18:46:42 +0600

    if.js

---
+++ if.js
@@ -0,0 +1,20 @@
+var a = 10
+var b = 20
+
+if(a>b){
+
+  console.log('A is greater than B')
+
+}
+
+if (a<b){
+    console.log('B is greater than A')
+}
+
+var n = 50
+if(n%2 === 0){
+    console.log(n + 'is even number')
+}
+if(n%2 ===1){
+    console.log(n + 'is odd number')
+}
