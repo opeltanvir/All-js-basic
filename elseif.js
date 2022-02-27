1f9b3557c796d9e11181a4a5765ae35d14dc8944
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-02-27 23:35:13 +0600

    elseif

---
+++ elseif.js
@@ -0,0 +1,23 @@
+var a = 20
+var b = 20
+
+if(a>b){
+
+  console.log('A is greater than B')
+
+} else if(a<b){
+    console.log('B is greater than A')
+}
+else{
+    console.log('both are same')
+}
+
+var n = 0
+if(n===0){
+    console.log(n ='is zero')
+}
+else if(n%2 === 0){
+    console.log(n + 'is even number')
+} else{
+    console.log(n + 'is odd number')
+}
