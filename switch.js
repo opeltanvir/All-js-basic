3017086badec6f10db4952ab26b2481907b7d3fd
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-03-02 23:43:54 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,16 @@
+var date = new Date()
+var today = date.getDay
+switch (2){
+    case 0:
+    console.log('Today is sunday')
+    break
+    case 1:
+    console.log('Today is monday')
+    break
+    case 2:
+    console.log('Today is Tuesday')
+    break
+
+    default: console.log('not a valid number')
+    
+}
