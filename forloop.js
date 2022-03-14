25896c72371e1b0cd62c6c5a4af2e4edf5afbd88
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-03-14 22:20:28 +0600

    for

--- forloop.js
+++ forloop.js
@@ -4,4 +4,14 @@ for(var i =0;i<1000;i++){
 }
 for(vari=1;i<= 100;i++){
     if(i%2 ===1)
-}console.log(;)
+}
+{console.log(i)
+}
+
+var sum = 0;
+for (var i=1; i<=10;i++){
+    console.log(sum+'+'+i+'='+(sum))
+    sum +=i
+}
+console.log('result'=+sum)
+
