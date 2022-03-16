c56c502cd19352ee8e3128c66124b628ad95b6af
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-03-16 22:59:28 +0600

    while loop

---
+++ Whileloop.js
@@ -0,0 +1,10 @@
+var isRanning =true
+while(isRanning){
+    var road =with.floor(Math.random()*10 + 1)
+    if(rand ===9){
+        console.log('winner winner chiecken dinner')
+        isRanning = false
+    }
+}else{
+    console.log('you have get'+rand)
+}
