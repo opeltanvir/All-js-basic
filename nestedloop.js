82fc134f44b6243a0a26c5af6376ff15b5e2fd7d
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-03-20 10:34:17 +0600

    loop

--- nestedloop.js
+++ nestedloop.js
@@ -6,3 +6,11 @@ for(var i=1; i<=n; i++){
  }    
  console.log{result}
 }
+var n= 10
+for(i=1;i<=n;i++){
+    var result=''
+    for(var j=1;j<=n;j++){
+        result + ='*'
+    }
+    console.log(result)
+}
