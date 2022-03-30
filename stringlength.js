b86c55abd310ee17fc2b50492dec8b572b483d30
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-03-30 23:51:32 +0600

    string leanght

---
+++ stringlength.js
@@ -0,0 +1,11 @@
+var str = 'tanvir'
+var str2 = 0
+while (true) {
+    if(str.charAt(length) ==''){
+        break
+    }else{
+        length++
+    }
+}
+console.log(str.length)
+console.log(bchhfnxc.length)
